b0413c6a58bf9aca5bd2e7d144b8207b72346e46
JanefrancessC <janefrancesschioma@gmail.com>
2023-10-18 17:01:28 -0700

    read_hist_all.c

---
+++ read_hist_all.c
@@ -0,0 +1,30 @@
+#include "shell.h"
+
+/**
+ * read_hist_all - Retrieve history list
+ * @info: Data param
+ * Return: String containing the history file paths.
+ */
+char *read_hist_all(info_t *info)
+{
+	char *homeDir, *historyPath;
+
+	homeDir = custom_getenv(info, "HOME=");
+	if (!homeDir)
+	{
+		return (NULL);
+	}
+
+	historyPath = malloc(sizeof(char) * (_strlen(homeDir) + _strlen(HISTORY_FILE) + 2));
+	if (!historyPath) {
+		return (NULL);
+	}
+
+	historyPath[0] = 0;
+	custom_cpy_str(historyPath, homeDir);
+	_strcat(historyPath, "/");
+	_strcat(historyPath, HISTORY_FILE);
+
+	return (historyPath);
+}
+
