ab19501e676f203dbfa4a87b9b24ca8c83bd10c9
JanefrancessC <janefrancesschioma@gmail.com>
2023-10-18 19:24:44 -0700

    indentations

--- add_env.c
+++ add_env.c
@@ -9,34 +9,34 @@
  */
 int add_env(info_t *info, char *vars_, char *value)
 {
-    char *buffer = NULL, *p;
-    list_t *node;
+	char *buffer = NULL, *p;
+	list_t *node;
 
-    if (!vars_ || !value)
-        return 0;
+	if (!vars_ || !value)
+		return (0);
 
-    buffer = malloc(_strlen(vars_) + _strlen(value) + 2);
-    if (!buffer)
-        return (1);
-    custom_cpy_str(buffer, vars_);
-    _strcat(buffer, "=");
-    _strcat(buffer, value);
-    node = info->env;
-    while (node)
-    {
-        p = char_finder(node->str, vars_);
-        if (p && *p == '=')
-        {
-            free(node->str);
-            node->str = buffer;
-            info->env_changed = 1;
-            free(buffer); 
-            return (0);
-        }
-        node = node->next;
-    }
-    add_node_end(&(info->env), buffer, 0);
-    free(buffer); 
-    info->env_changed = 1;
-    return (0);
+	buffer = malloc(_strlen(vars_) + _strlen(value) + 2);
+	if (!buffer)
+		return (1);
+	custom_cpy_str(buffer, vars_);
+	_strcat(buffer, "=");
+	_strcat(buffer, value);
+	node = info->env;
+	while (node)
+	{
+		p = char_finder(node->str, vars_);
+		if (p && *p == '=')
+		{
+			free(node->str);
+			node->str = buffer;
+			info->env_changed = 1;
+			free(buffer);
+			return (0);
+		}
+		node = node->next;
+	}
+	add_node_end(&(info->env), buffer, 0);
+	free(buffer);
+	info->env_changed = 1;
+	return (0);
 }
--- add_history_list.c
+++ add_history_list.c
@@ -4,22 +4,21 @@
  * add_history_list - To add an entry to history.
  * @info: Data param.
  * @buf: Buffer.
- * @lineCount: The history line count.
+ * @linecount: The history line count.
  * Return: 0 for success.
  */
 
-int add_history_list(info_t *info, char *buf, int linecount) {
-    list_t *node = NULL;
+int add_history_list(info_t *info, char *buf, int linecount)
+{
+	list_t *node = NULL;
 
-    if (info->history) {
-        node = info->history;
-    }
+	if (info->history)
+		node = info->history;
 
-    add_node_end(&node, buf, linecount);
+	add_node_end(&node, buf, linecount);
 
-    if (!info->history) {
-        info->history = node;
-    }
+	if (!info->history)
+		info->history = node;
 
-    return (0);
+	return (0);
 }
