9101d084eff125e4c9c882f68b9d5c38c4822466
JanefrancessC <janefrancesschioma@gmail.com>
2023-10-20 08:07:07 -0700

    betty decription for print_single_alias

--- print_single_alias.c
+++ print_single_alias.c
@@ -1,5 +1,11 @@
 #include "shell.h"
 
+/**
+ * print_single_alias - Print a single alias to an output
+ * @node: pointer to node in a linked list containing alias strings
+ * Return: 0 on success and 1 if node is NULL
+ */
+
 int print_single_alias(list_t *node)
 {
 	char *equal_sign = NULL;
